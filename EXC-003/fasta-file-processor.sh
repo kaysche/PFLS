e066fda142f11a5a13ded9c342210538046623f5
kaysche <kay.scheumann@gmx.de>
2026-02-09 10:15:40 +0100

    Correction 3

--- EXC-003/fasta-file-processor.sh
+++ EXC-003/fasta-file-processor.sh
@@ -1,4 +1,5 @@
 ##test for right order in file
+i
 if [ "$(head -c 1 $1)" == ">" ]; then 
 
 ## FASTA file all sequence in one line, new file created
@@ -9,16 +10,16 @@ num_contigs=$( grep '>' genes-one-line.fna | wc -l | awk '{print $1}' )
 total_length_sq=$(grep -v '>' genes-one-line.fna | tr -d '\n' | wc -c)
 
 ## Length of the longest sequence 
-long=$(cat genes-one-line.fna | grep -v '>' "$one_line" |  awk '{print length}' | sort -n | tail -n 1) 
+long=$(cat genes-one-line.fna | grep -v '>' genes-one-line.fna |  awk '{print length}' | sort -n | tail -n 1) 
 
 ## length of shortest sequence 
-short=$(cat genes-one-line.fna | grep -v '>' "$one_line" | awk '{print length}' | sort -n | head -n 1)
+short=$(cat genes-one-line.fna | grep -v '>' genes-one-line.fna | awk '{print length}' | sort -n | head -n 1)
 
 ## average length of sequences
 aveg=$(($total_length_sq/$num_contigs))
 
 ## GC Content (%)
-GC_amount=$(cat genes-one-line.fna | grep -v '>' "$one_line"| awk '{gc_count += gsub(/[GgCc]/, "", $1)} END {print gc_count}')
+GC_amount=$(cat genes-one-line.fna | grep -v '>' genes-one-line.fna | awk '{gc_count += gsub(/[GgCc]/, "", $1)} END {print gc_count}')
 GC=$(($GC_amount * 100/$total_length_sq))
 GC_=$(echo "$GC" | bc)           ## extra step for xx.x percentage (my system does not have it installed)
 
